d5457f08843f360f66387c32dc7f9467cb022c73
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2023-03-10 23:56:54 -0300

    vercel config

--- src/adapters/http/middlewares/useRateLimit.ts
+++ src/adapters/http/middlewares/useRateLimit.ts
@@ -1,7 +1,7 @@
 import rateLimit  from 'express-rate-limit'
 
 export const rateLimiter = () => rateLimit({ 
-    max: 100,
-    windowMs: 60 * 60 * 1000,
-    message: {message: 'Muitas requisições a partir deste IP, tente novamente após 60 minutos' }
+    max: 120,
+    windowMs: 10 * 60 * 1000,
+    message: {message: 'Muitas requisições a partir deste IP, tente novamente após 10 minutos' }
   });
